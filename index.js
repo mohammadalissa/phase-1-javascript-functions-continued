2e593eda807c32cc655f7c44a08ab414ba6156c2
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-10-10 17:26:15 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const saturdayFun =  function (st ='roller-skate'){
+        return `This Saturday, I want to ${st}!`
+}
+let mondayWork = function(st='go to the office'){
+    return `This Monday, I will ${st}.`
+}
+
+function wrapAdjective(stri='*'){
+    function inner(para = 'special'){
+        return `You are ${stri}${para}${stri}!`
+    }
+    return inner
+}
+console.log(wrapAdjective("%")("a dedicated programmer"))
